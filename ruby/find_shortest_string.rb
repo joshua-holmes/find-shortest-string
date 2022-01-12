61b7a2458160154aa0dfc4d98445faa13a777115
Joshua Holmes <joshua.phillip.holmes@gmail.com>
2022-01-11 22:56:01 -0600

    Completed assignments

--- ruby/find_shortest_string.rb
+++ ruby/find_shortest_string.rb
@@ -1,5 +1,16 @@
 def find_shortest_string(arr)
-  # type your code in here
+
+  lengths = arr.map { |s| s.length }
+  min = lengths.min
+  index = lengths.find_index min
+  arr[index]
+
+  ## This version was 28% slower
+  # shortest = nil
+  # arr.each_with_index do |s, i|
+  #   shortest = s if i == 0 || s.length < shortest.length
+  # end
+  # shortest
 end
 
 if __FILE__ == $PROGRAM_NAME
@@ -19,7 +30,20 @@ if __FILE__ == $PROGRAM_NAME
   # Don't forget to add your own!
 
   # BENCHMARK HERE
+  strings = ['flower', 'juniper', 'lily', 'dadelion']
+
+  before = Time.now
+  1000000.times { find_shortest_string strings }
+  after = Time.now
+
+  puts "#{after - before} seconds"
 end
 
 # Please add your pseudocode to this file
 # And a written explanation of your solution
+=begin 
+map over original array to get array of lengths
+find min of that arr
+find index of that min
+return shortest letter using index and original array
+=end
